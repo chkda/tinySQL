b58459f8804e4749bab9909ad5dd02be92af009e
Chhaya <chayakudas.das56@gmail.com>
2024-07-01 12:50:31 +0530

    Adding frontend

--- main.go
+++ main.go
@@ -7,6 +7,31 @@ import (
 	"strings"
 )
 
+type MetaCommandResult int
+
+const (
+	META_COMMAND_SUCCESS MetaCommandResult = iota
+	META_UNRECOGNISED_COMMAND
+)
+
+type PrepareResult int
+
+const (
+	PREPARE_SUCCESS PrepareResult = iota
+	PREPARE_UNRECOGNISED_COMMAND
+)
+
+type StatementType int
+
+const (
+	STATEMENT_INSERT StatementType = iota
+	STATEMENT_SELECT
+)
+
+type Statement struct {
+	typ StatementType
+}
+
 type InputBuffer struct {
 	buffer       string
 	bufferLength int
@@ -29,9 +54,39 @@ func readInput(inputBuffer *InputBuffer) {
 		os.Exit(1)
 	}
 	inputBuffer.bufferLength = len(buffer) - 1
-	buffer = strings.TrimPrefix(buffer, "\n")
-	buffer = strings.TrimSuffix(buffer, "\n")
+	// buffer = strings.TrimPrefix(buffer, "\n")
+	// buffer = strings.TrimSuffix(buffer, "\n")
+	buffer = strings.TrimSpace(buffer)
 	inputBuffer.buffer = buffer
+}
+
+func doMetaCommand(inputBuffer *InputBuffer) MetaCommandResult {
+	if inputBuffer.buffer == ".exit" {
+		os.Exit(0)
+		return META_COMMAND_SUCCESS
+	}
+	return META_UNRECOGNISED_COMMAND
+}
+
+func prepareStatement(inputBuffer *InputBuffer, statement *Statement) PrepareResult {
+	if strings.HasPrefix(inputBuffer.buffer, "insert") {
+		statement.typ = STATEMENT_INSERT
+		return PREPARE_SUCCESS
+	}
+	if strings.HasPrefix(inputBuffer.buffer, "select") {
+		statement.typ = STATEMENT_SELECT
+		return PREPARE_SUCCESS
+	}
+	return PREPARE_UNRECOGNISED_COMMAND
+}
+
+func executeStatement(statement *Statement) {
+	switch statement.typ {
+	case STATEMENT_INSERT:
+		fmt.Println("TODO: Insert statement execution")
+	case STATEMENT_SELECT:
+		fmt.Println("TODO: Select statement execution")
+	}
 }
 
 func main() {
@@ -39,10 +94,26 @@ func main() {
 	for {
 		printPrompt()
 		readInput(inputBuffer)
-		if inputBuffer.buffer == ".exit" {
-			break
-		} else {
-			fmt.Printf("Unrecognised command: %s\n", inputBuffer.buffer)
+		if strings.HasPrefix(inputBuffer.buffer, ".") {
+			switch doMetaCommand(inputBuffer) {
+			case META_COMMAND_SUCCESS:
+				continue
+			case META_UNRECOGNISED_COMMAND:
+				fmt.Printf("Unrecognised Command: %s\n", inputBuffer.buffer)
+				continue
+			}
 		}
+
+		statement := &Statement{}
+		switch prepareStatement(inputBuffer, statement) {
+		case PREPARE_SUCCESS:
+			break
+		case PREPARE_UNRECOGNISED_COMMAND:
+			fmt.Printf("Unrecognised Command: %s\n", inputBuffer.buffer)
+			continue
+		}
+
+		executeStatement(statement)
+		fmt.Println("executed.")
 	}
 }
